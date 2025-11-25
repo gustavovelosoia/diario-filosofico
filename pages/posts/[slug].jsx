5576e90001ed5416f28f4790343b7452891706ca
gustavovelosoia <emergiagust@gmail.com>
2025-11-25 16:42:33 -0300

    Update [slug].jsx

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -54,7 +54,7 @@ export default function Post({ post }) {
           {/* Markdown Content */}
           <div 
             className={styles.content}
-dangerouslySetInnerHTML={{ __html: post.contentHtml.replace(/<h1>.*?<\/h1>/s, '') }}          />
+dangerouslySetInnerHTML={{ __html: post.contentHtml. }}          />
         </article>
 
         {/* Footer */}
