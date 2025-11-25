c5f8fc5dad753caf57b978842ef2ecd21a9ab485
gustavovelosoia <emergiagust@gmail.com>
2025-11-25 11:10:01 -0300

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import '../styles/globals.css';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Diário Filosófico - Reflexões contemplativas sobre correntes filosóficas, pensadores clássicos e modernos em atmosfera dark academia." />
+        <meta name="theme-color" content="#0f0f1e" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
